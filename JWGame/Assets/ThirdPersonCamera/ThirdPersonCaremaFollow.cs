1d350cc3c804c9ee77edecd75a41935a7894007a
vladimirpetrenko <kings02VP2015>
2018-08-21 11:47:51 +0200

    char camera  controller

---
+++ JWGame/Assets/ThirdPersonCamera/ThirdPersonCaremaFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCaremaFollow : MonoBehaviour {
+
+	public float CameraMoveSpeed = 120.0f;
+	public GameObject cameraFollower;
+	public Vector3 followPosition;
+	public float clampAngle =0.0f;
+	public float inputSensibility =150.0f;
+	public GameObject cameraObject;
+	public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
